d31dcff125a8ab6abe9f569c0ce3fbb6e9bbace0
blanpainfrancois <blanpainfrancois@gmail.com>
2017-03-23 08:50:28 +0100

    first commit

---
+++ Challenge2_navigation/Challenge2_navigation/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Challenge2_navigation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_GoToPage1(object sender, EventArgs e)
+        {
+             
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private async void Button_GoToPage2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
